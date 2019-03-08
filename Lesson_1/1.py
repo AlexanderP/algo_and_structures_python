5d45483c72419ddec83d48ed22e1860f57e475c2
Alexander Pozdnyakov <almipo@mail.ru>
2019-03-08 20:17:33 +0300

    Добавил сумму

--- Lesson_1/1.py
+++ Lesson_1/1.py
@@ -6,5 +6,7 @@ number_0 = number // 100
 number_2 = number % 10
 number_1 = (number - number_2 - number_0 * 100) / 10
 number_4 = number_0 * number_1 * number_2
+number_5 = number_0 + number_1 + number_2
 
-print(f"Произведение цифр трех значного числа {number} равно: {number_4}")
+print(f"Произведение цифр трех значного числа {number} равна: {number_4}")
+print(f"Сумма цифр трех значного числа {number} равна: {number_5}")
