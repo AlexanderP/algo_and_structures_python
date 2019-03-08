f79cb3908a9ad2bc93e4f89b72574439d9fedb97
Alexander Pozdnyakov <almipo@mail.ru>
2019-03-08 20:06:34 +0300

    Первый урок

--- Lesson_1/1.py
+++ Lesson_1/1.py
@@ -1,1 +1,10 @@
 # 1.	Найти сумму и произведение цифр трехзначного числа, которое вводит пользователь.
+
+number = int(input("Введите трех значное число: "))
+
+number_0 = number // 100
+number_2 = number % 10
+number_1 = (number - number_2 - number_0 * 100) / 10
+number_4 = number_0 * number_1 * number_2
+
+print(f"Произведение цифр трех значного числа {number} равно: {number_4}")
--- Lesson_1/3.py
+++ Lesson_1/3.py
@@ -1,2 +1,28 @@
 # 3.	По введенным пользователем координатам двух точек вывести
 # уравнение прямой вида y = kx + b, проходящей через эти точки.
+
+ax, ay = input("Введите координаты точки А через пробел: ").split()
+bx, by = input("Введите координаты точки B через пробел: ").split()
+
+A = int(ay) - int(by)
+B = int(bx) - int(ax)
+C = int(ax) * int(by) - int(bx) * int(ay)
+
+if ax == bx and ay == by:
+    print("Координаты точек равны")
+else:
+    if B < 0:
+        B = f" - {abs(B)}y"
+    else:
+        B = f" + {abs(B)}y"
+    if A < 0:
+        A = f" - {abs(A)}x"
+    else:
+        A = f"{abs(A)}x"
+    if C == 0:
+        C = ''
+    elif C < 0:
+        C = f" - {abs(C)}"
+    else:
+        C = f" + {abs(C)}"
+    print(f"Уравнение прямой: {A}{B}{C}")
