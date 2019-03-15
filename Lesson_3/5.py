648a52a617442ec37ffb117f99258ee727f10594
Alexander Pozdnyakov <almipo@mail.ru>
2019-03-15 21:37:59 +0300

    Мелкие правки

--- Lesson_3/5.py
+++ Lesson_3/5.py
@@ -2,16 +2,22 @@
 # Вывести на экран его значение и позицию (индекс) в массиве.
 import random
 
-a = -109
+a = None
 n = ''
 
 array = [random.randint(-100, 100) for _ in range(40)]
 
 for i, j in enumerate(array):
     if j < 0:
-        if a < j:
+        if a is None:
+            n = i
+            a = j
+        elif a < j:
             n = i
             a = j
 
-print(f"В массиве:\n{array}\nМаксимальный отрицательный элемент {a} - "
-      f"Находится на позиции {n}")
+if a is None:
+    print(f"В массиве:\n{array}\nНет отрицательных элементов")
+else:
+    print(f"В массиве:\n{array}\nМаксимальный отрицательный элемент {a} - "
+          f"Находится на позиции {n}")
